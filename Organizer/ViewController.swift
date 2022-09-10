ac6d11b0c421db67dfa67468f76c3703fa3a176b
Pavel <pavel.818@yandex.ru>
2022-09-10 12:00:18 +0400

    Initial Commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Pavel Абрамов on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
